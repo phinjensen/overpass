1495245fc9c1c7d27d9e005afd24239aeac4627a
Phineas Jensen <phin@zayda.net>
2023-03-26 13:37:00 -0600

    Add MultiPolygon conversion for Relation types

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
 pub mod polygon_feature;
 
-use std::{borrow::Cow, collections::HashMap, convert::TryInto};
+use std::{borrow::Cow, collections::HashMap};
 
-use geo_types::{coord, Coord, Geometry, LineString, Point, Polygon};
+use geo_types::{coord, Coord, Geometry, LineString, MultiPolygon, Point, Polygon};
 use polygon_feature::{Rule, POLYGON_FEATURE_RULES};
 use serde::{Deserialize, Serialize};
 
 type WayNodes = Vec<u64>;
 
 #[derive(Serialize, Deserialize)]
-struct OSM3S {
+pub struct OSM3S {
     timestamp_osm_base: String,
     timestamp_areas_base: String,
     copyright: String,
 }
 
 #[derive(Clone, Serialize, Deserialize)]
-struct RelationMember {
+pub struct RelationMember {
     #[serde(rename = "type")]
     _type: String,
     #[serde(rename = "ref")]
@@ -26,7 +26,7 @@ struct RelationMember {
 
 #[derive(Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
-enum ElementType {
+pub enum ElementType {
     #[serde(rename = "node")]
     Node { lat: f64, lon: f64 },
     #[serde(rename = "way")]
@@ -36,7 +36,7 @@ enum ElementType {
 }
 
 #[derive(Clone, Serialize, Deserialize)]
-struct Element {
+pub struct Element {
     #[serde(rename = "type", flatten)]
     _type: ElementType,
     id: u64,
@@ -45,7 +45,7 @@ struct Element {
 }
 
 #[derive(Serialize, Deserialize)]
-struct Response {
+pub struct Response {
     version: f32,
     generator: String,
     osm3s: OSM3S,
@@ -105,18 +105,18 @@ fn is_polygon_feature(tags: &HashMap<String, String>) -> bool {
 
 /// Returns disparate ways joined into LineStrings (including closed rings where possible)
 fn join_ways(
-    ways: Vec<Element>,
+    ways: Vec<&Element>,
     node_map: &HashMap<u64, Cow<Element>>,
 ) -> Result<Vec<LineString>, Error> {
     let mut result: HashMap<u64, Vec<Coord>> = HashMap::new();
     for way in ways {
-        if let ElementType::Way { nodes } = way._type {
+        if let ElementType::Way { nodes } = &way._type {
             match (nodes.first(), nodes.last()) {
                 (Some(first), Some(last)) => {
                     if first == last {
-                        result.insert(*last, get_nodes(&nodes, node_map)?);
+                        result.insert(*last, get_coords(&nodes, node_map)?);
                     } else if let Some(coords) = result.get_mut(first) {
-                        coords.extend_from_slice(&get_nodes(&nodes, node_map)?[1..]);
+                        coords.extend_from_slice(&get_coords(&nodes, node_map)?[1..]);
                     }
                 }
                 _ => (),
@@ -129,7 +129,10 @@ fn join_ways(
         .collect())
 }
 
-fn get_nodes(nodes: &WayNodes, node_map: &HashMap<u64, Cow<Element>>) -> Result<Vec<Coord>, Error> {
+fn get_coords(
+    nodes: &WayNodes,
+    node_map: &HashMap<u64, Cow<Element>>,
+) -> Result<Vec<Coord>, Error> {
     nodes
         .iter()
         .map(
@@ -143,6 +146,33 @@ fn get_nodes(nodes: &WayNodes, node_map: &HashMap<u64, Cow<Element>>) -> Result<
         .collect()
 }
 
+// Calculate whether a point falls within the polygon denoted by a LineString
+fn coord_in_polygon_ls(p: &Coord, polygon: &LineString) -> bool {
+    if !polygon.is_closed() {
+        panic!("LineString for point_in_polygon must be closed!");
+    }
+    let mut inside = false;
+    for j in 1..polygon.points().len() {
+        let ci = &polygon.0[j - 1];
+        let cj = &polygon.0[j];
+        if ((ci.y > p.y) != (cj.y > p.y))
+            && (p.x < (cj.x - ci.x) * (p.y - ci.y) / (cj.y - ci.y) + ci.x)
+        {
+            inside = !inside;
+        }
+    }
+    inside
+}
+
+fn polygons_intersect_ls(outer: &LineString, inner: &LineString) -> bool {
+    for coord in inner.coords() {
+        if coord_in_polygon_ls(coord, outer) {
+            return true;
+        }
+    }
+    false
+}
+
 impl Element {
     /// Converts a given Overpass API element into the appropriate geo_type.
     ///
@@ -152,12 +182,12 @@ impl Element {
     ///     - into a LineString otherwise
     /// - A Relation is converted
     ///
-    fn to_geo(&self, element_map: &HashMap<u64, Cow<Element>>) -> Result<Geometry, Error> {
+    pub fn to_geo(&self, element_map: &HashMap<u64, Cow<Element>>) -> Result<Geometry, Error> {
         match &self._type {
             ElementType::Node { lat, lon } => Ok(Point::new(*lon, *lat).into()),
             ElementType::Way { nodes } => {
                 // A way should be composed of nodes that we can convert to geo_types::Coord
-                let points = get_nodes(nodes, element_map)?;
+                let points = get_coords(nodes, element_map)?;
 
                 // A way should also be longer than one node; otherwise it's not a string or
                 // polygon
@@ -175,6 +205,59 @@ impl Element {
                 Ok(line.into())
             }
             ElementType::Relation { members } => {
+                // Convert a relation into a MultiPolygon if it has the tag type=multipolygon or type=boundary
+                if let Some(tags) = &self.tags {
+                    let type_tag = match tags.get("type") {
+                        Some(t) => t.as_str(),
+                        _ => "",
+                    };
+                    if type_tag == "multipolygon" || type_tag == "boundary" {
+                        // A multipolygon relation should have at least one outer member
+                        let outer_members = members
+                            .iter()
+                            .filter(|member| member.role == "outer")
+                            .collect::<Vec<_>>();
+                        if outer_members.is_empty() {
+                            return Err(Error::InvalidData);
+                        }
+
+                        // Each outer member should be a way
+                        let outer_ways = outer_members
+                            .iter()
+                            .filter_map(|member| element_map.get(&member._ref))
+                            .map(|e| e.as_ref())
+                            .collect::<Vec<_>>();
+
+                        // Each outer way should be a closed ring
+                        let mut polygons = join_ways(outer_ways, element_map)?
+                            .into_iter()
+                            .map(|ring| Polygon::new(ring, vec![]))
+                            .collect::<Vec<_>>();
+
+                        let inner_ways = members
+                            .iter()
+                            .filter(|member| member.role == "inner")
+                            .filter_map(|member| element_map.get(&member._ref))
+                            .map(|e| e.as_ref())
+                            .collect::<Vec<_>>();
+
+                        // Each outer way should be a closed ring
+                        let inner_rings = join_ways(inner_ways, element_map)?;
+                        if inner_rings.iter().any(|ring| ring.is_closed()) {
+                            return Err(Error::InvalidData);
+                        }
+
+                        for polygon in &mut polygons {
+                            for ring in &inner_rings {
+                                if polygons_intersect_ls(&polygon.exterior(), &ring) {
+                                    polygon.interiors_push(ring.clone())
+                                }
+                            }
+                        }
+                        return Ok(MultiPolygon::new(polygons).into());
+                    }
+                }
+
                 // ignore if no members
                 //
                 // if type=route or type=waterway
